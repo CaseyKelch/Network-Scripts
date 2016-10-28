c3edc9eb7335b01404f24d97b1e8f89d64b53e47
Casey Kelch <ckelc206@gmail.com>
2016-10-28 11:19:17 -0500

    Update F5_UCS_Backup.py

--- F5_UCS_Backup.py
+++ F5_UCS_Backup.py
@@ -3,66 +3,72 @@
 import paramiko
 import time
 import datetime
-import smtplib
 import base64
 from collections import OrderedDict
 
 
+class F5Backup:
+
+    def __init__(self, sorteddict, username, password, directory):
+        self.sorteddict = sorteddict
+        self.username = username
+        self.password = password
+        self.directory = directory
+
+    """
+    UCS Backup Creation
+    """
+
+    def archive_ucs(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        for ip, f5host in self.sorteddict.items():
+            ssh.connect(ip, username=self.username, password=self.password)
+            ssh.exec_command("tmsh save /sys ucs /var/tmp/"+f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs")
+            time.sleep(30)
+            ssh.close()
+
+    """
+    SFTP UCS file from F5 to Directory
+    """
+
+    def sftp_ucs(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        for ip, f5host in self.sorteddict.items():
+            ssh.connect(ip, username=self.username, password=self.password)
+            sftp = ssh.open_sftp()
+            sftp.get("/var/tmp/"+f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs", self.directory +
+                     f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs")
+            sftp.close()
+            ssh.close()
+
+    """
+    Cleanup UCS files from F5 /var/tmp directory.
+    """
+
+    def cleanup_ucs(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        for ip, f5host in self.sorteddict.items():
+            ssh.connect(ip, username=self.username, password=self.password)
+            ssh.exec_command("rm /var/tmp/*.ucs")
+            time.sleep(2)
+            ssh.close()
+
+
 """
-Generate UCS backup
+-Sample-
 """
-
-
-def backup_ucs(sorteddict):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    for ip, f5host in sorteddict.items():
-        ssh.connect(ip, username=username, password=password)
-        ssh.exec_command("tmsh save /sys ucs /var/tmp/"+f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs")
-        time.sleep(30)
-        ssh.close()
-
-"""
-SFTP UCS file from F5 to NEESRV5012
-"""
-
-
-def sftp_ucs(sorteddict):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    for ip, f5host in sorteddict.items():
-        ssh.connect(ip, username=username, password=password)
-        sftp = ssh.open_sftp()
-        sftp.get("/var/tmp/"+f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs", "C:\\"+
-                 f5host+"{:%m-%d-%Y}".format(datetime.datetime.now())+".ucs")
-        sftp.close()
-        ssh.close()
-
-"""
-Cleanup UCS files from F5 /var/tmp directory.
-"""
-
-
-def cleanup_ucs(sorteddict):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    for ip, f5host in sorteddict.items():
-        ssh.connect(ip, username=username, password=password)
-        ssh.exec_command("rm /var/tmp/*.ucs")
-        time.sleep(2)
-        ssh.close()
-
-            
-# Credentials
-username = 'admin'
-password = base64.b64decode('xxxxx').decode('ascii')
-
-# Dictionary with IP as key with Hostname value
 f5_dict = {
             "1.1.1.1": "F5_Host_1", "1.1.1.2": "F5_Host_2"
            }
-
-# Sorts dictionary so that results are consistent.  Orders by IP address(Key).
 f5_sorted = OrderedDict(sorted(f5_dict.items()))
+user = 'admin'
+passw = base64.b64decode('xxxxx').decode('ascii')
+my_dir = "C:\\Desktop\\F5"
 
-
+backup = F5Backup(f5_sorted, user, passw, my_dir)
+backup.archive_ucs()
+backup.sftp_ucs()
+backup.cleanup.ucs()
